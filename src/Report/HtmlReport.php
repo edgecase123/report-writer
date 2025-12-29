f6e76aaac5ec69f21b1c196f70adbf04dc305697
Lee Jenkins <lee.jenkins@foreup.com>
2025-12-29 10:25:19 -0500

    main Removed unnecessary circular reference between HtmlReport and AbstractReport

--- src/Report/HtmlReport.php
+++ src/Report/HtmlReport.php
@@ -9,24 +9,19 @@ class HtmlReport extends AbstractReport
 {
     protected ?RendererInterface $renderer;
 
-    private AbstractReport $report;
-
     public function __construct(
-        RendererInterface $renderer,
-        ?AbstractReport $report = null
+        RendererInterface $renderer
     ) {
         $this->renderer = $renderer;
-        $this->report = $report ?? $this;
+
+        if ($this->renderer instanceof HtmlTableRenderer) {
+            $this->renderer->setReport($this);
+        }
     }
 
     protected function renderBand(string $type, ?int $level = null, $context = null): string
     {
-        // If renderer is HtmlTableRenderer, inject the report so it can access columns
-        if ($this->renderer instanceof HtmlTableRenderer) {
-            $this->renderer->setReport($this->report);
-        }
-
-        $this->renderer->renderBand($type, $level, $context ?? []);
+        $this->getRenderer()?->renderBand($type, $level, $context ?? []);
 
         return '';
     }
@@ -35,6 +30,6 @@ class HtmlReport extends AbstractReport
     {
         parent::render();
 
-        return $this->renderer->getOutput();
+        return $this->getRenderer()?->getOutput() ?? '';
     }
 }
